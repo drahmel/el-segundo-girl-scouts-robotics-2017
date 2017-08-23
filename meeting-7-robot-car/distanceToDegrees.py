08808f2ca3f5fa4e9eef4ff4fea33fe6a666e9a0
Dan Rahmel <drahmel7@gmail.com>
2017-08-22 18:43:24 -0700

    DJR:Added presentation and basic program

---
+++ meeting-7-robot-car/distanceToDegrees.py
@@ -0,0 +1,12 @@
+
+def distanceToDegrees(distance):
+	pi = 3.14159
+	diameter = 1
+	circumference = diameter * pi
+	print("circumference", circumference)
+	rotations = distance / circumference
+	print("rotations", rotations)
+	degrees = rotations * 360
+	print("degrees", degrees)
+
+distanceToDegrees(10)
